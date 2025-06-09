02c4b86f46777509e1ce3c74912f3c384498618e
lescalonag-dlai <luis.galvis@deeplearning.ai>
2025-06-09 19:21:06 -0400

    Update setup.sql

--- M2/Lab1/setup.sql
+++ M2/Lab1/setup.sql
@@ -11,17 +11,6 @@ CREATE SCHEMA IF NOT EXISTS avalanche_schema;
 CREATE STAGE IF NOT EXISTS avalanche_db.avalanche_schema.customer_reviews
   ENCRYPTION = (TYPE = 'SNOWFLAKE_SSE')
   DIRECTORY = (ENABLE = true);
---
--- Now go and upload files to the stage. 
--- Once you've done that proceed to the next step
-
--- Option 2: Push files to Stage from S3
--- Uncomment lines below to use:
---
--- Create the stage for storing our files
--- CREATE OR REPLACE STAGE avalanche_db.avalanche_schema.customer_reviews
-   -- URL = 's3://sfquickstarts/misc/customer_reviews/'
-   -- DIRECTORY = (ENABLE = TRUE AUTO_REFRESH = TRUE);
 
 
 -- STEP 3
