9754d914d84da16ddf962a2a06f35dee57d1aa72
Summer Rae <summer@deeplearning.ai>
2025-06-12 20:28:31 -0400

    updated M1/Lab1 Notebook

--- M1/Lab2/streamlit_app.py
+++ M1/Lab2/streamlit_app.py
@@ -1,7 +1,27 @@
 import streamlit as st
 import pandas as pd
 
+# app title
 st.title("🏔️ Avalanche Data Set")
 
+# read in and display dataframe
 df = pd.read_csv("data/customer_reviews.csv")
-df
+df.head()
+
+# Drop-down widget
+unique_products = sorted(df.PRODUCT.unique())
+selected_products = st.multiselect('Select product(s)', unique_products, unique_products)
+st.write('Selected products:', list(selected_products))
+df[df.PRODUCT.isin(selected_products)]
+
+# Calendar widget
+st.subheader('Dates')
+
+df['DATE'] = pd.to_datetime(df['DATE'])
+
+d = st.date_input(
+    "Select dates",
+    (df['DATE'].min().date(), df['DATE'].median().date() ),
+    df['DATE'].min().date(),
+    df['DATE'].max().date(),
+)
