563d2f380c8c16f86cc2407cf7cb982b37404302
lescalonag-dlai <luis.galvis@deeplearning.ai>
2025-06-15 07:41:42 -0400

    Update streamlit_app.py

--- M1/Lab2/streamlit_app.py
+++ M1/Lab2/streamlit_app.py
@@ -9,19 +9,6 @@ df = pd.read_csv("data/customer_reviews.csv")
 df.head()
 
 # Drop-down widget
-unique_products = sorted(df.PRODUCT.unique())
-selected_products = st.multiselect('Select product(s)', unique_products, unique_products)
-st.write('Selected products:', list(selected_products))
-df[df.PRODUCT.isin(selected_products)]
+
 
 # Calendar widget
-st.subheader('Dates')
-
-df['DATE'] = pd.to_datetime(df['DATE'])
-
-d = st.date_input(
-    "Select dates",
-    (df['DATE'].min().date(), df['DATE'].median().date() ),
-    df['DATE'].min().date(),
-    df['DATE'].max().date(),
-)
